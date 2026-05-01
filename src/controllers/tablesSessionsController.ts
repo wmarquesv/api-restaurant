483b405f52f936925c5c1a2446bab51f931cb2ee
wmarquesv <walace.mv@hotmail.com>
2026-04-30 21:36:46 -0300

    organizado pasta de seeds, adicionado com o metodo post a adicao de abertura da mesa para disponibilizar os servicos

--- src/controllers/tablesSessionsController.ts
+++ src/controllers/tablesSessionsController.ts
@@ -1,8 +1,23 @@
+import { table } from "console";
 import { Request, Response, NextFunction } from "express";
+import { knex } from "@/database/knex.js";
+
+import { z } from "zod";
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
+      const bodySchema = z.object({
+        table_id: z.number(),
+      });
+
+      const { table_id } = bodySchema.parse(request.body);
+
+      await knex<TablesSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
       return response.status(201).json();
     } catch (error) {
       next(error);
